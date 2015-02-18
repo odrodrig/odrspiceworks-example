5ddd23b30e896ee35df6896972cca5ff2c88b250
Michael Gerbush <michaelg@spiceworks.com>
2015-02-18 17:33:54 -0600

    Update helpdesk service reference docs.
    For ticket requests, ‘medium’ is not a valid priority option, it must be
    ‘med’.
    Also, add the default priority for creating a new ticket, which is 
    always ‘med’.
    Update the ticket request stubs in the helpdesk service, to remove 
    needed requests and add the new ticket:create request.

--- host/helpdesk-service.js
+++ host/helpdesk-service.js
@@ -5,19 +5,9 @@ var HelpdeskService = Oasis.Service.extend({
     this.sandbox.helpdeskProxyPort = port;
   },
   requests: {
-    tickets: null,
-    unassigned: null,
-    open: null,
-    pastDue: null,
-    recent: null,
-    recentOpen: null,
-    closed: null,
-    assigned: null,
-    requiringPurchase: null,
-    email: null,
-    openAndAssignedTo: null,
-    openAndAssignedToCurrentUser: null,
-    assignedToUserAndHasNotBeenViewed: null
+    'tickets': null,
+    'ticket': null,
+    'ticket:create': null
   }
 });
 
