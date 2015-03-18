8a0264d11bcae3f8a554d084be82dbe21c16ff4d
Michael Gerbush <michaelg@spiceworks.com>
2015-03-18 07:02:22 -0500

    Add event to environment service for navigation
    Added a new event on the environment service for triggering a navigation
    of the host environment window.
    Added documentation.
    Renamed the ‘send’ event method on a Card object to ‘trigger’.
    Bumped the SDK version for the first time.  Going to continue doing 
    minor version bumps when the client SDK must be changed, until we hit 
    the 1.0 release, when we will begin following semver.

--- dist/spiceworks-sdk-host.js
+++ dist/spiceworks-sdk-host.js
@@ -1,5 +1,5 @@
 (function(global) {
-/*! spiceworks-sdk - v0.0.2 - 2015-03-06
+/*! spiceworks-sdk - v0.1.0 - 2015-03-18
 * http://developers.spiceworks.com
 * Copyright (c) 2015 ; Licensed  */
 var define, require;
@@ -4137,10 +4137,13 @@ define("environment-service",
       initialize: function () {
         this.send('activate', this.data);
       },
+      events: {
+        navigate: null
+      },
       requests: {
         environment: null,
         users: null,
-        user: null,
+        user: null
       },
       data: null //extend this property to send in data on activation
     });
--- dist/spiceworks-sdk.amd.js
+++ dist/spiceworks-sdk.amd.js
@@ -1,4 +1,4 @@
-/*! spiceworks-sdk - v0.0.2 - 2015-03-06
+/*! spiceworks-sdk - v0.1.0 - 2015-03-18
 * http://developers.spiceworks.com
 * Copyright (c) 2015 ; Licensed  */
 define("spiceworks-sdk", 
@@ -21,7 +21,7 @@ define("spiceworks-sdk/card-service",
     }
 
     CardService.prototype = {
-      send: function (event, data) {
+      trigger: function (event, data) {
         this.promise.then(function (port) {
           port.send(event, data);
         });
